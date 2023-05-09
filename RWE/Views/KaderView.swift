853fb18f5b1ce4254c4f838c8d2a4910d2742383
oki2207 <115452565+oki2207@users.noreply.github.com>
2023-05-09 08:39:26 +0200

    .....

--- RWE/Views/KaderView.swift
+++ RWE/Views/KaderView.swift
@@ -42,76 +42,65 @@ struct KaderView: View {
                 )
                 .padding(.horizontal, 20)
                 
-                    ScrollView {
-                        ForEach(kaderViewModel.kader, id: \.id) { spieler in
-                            ZStack {
+                ScrollView {
+                    ForEach(kaderViewModel.kader, id: \.id) { spieler in
+                        ZStack {
+                            VStack {
                                 HStack {
-                                    VStack {
-                                        
-                                        
-                                        Text("\(spieler.number)")
-                                            .font(.custom("SignPainter", size: 30))
-                                            .frame(maxWidth: 230, alignment: .trailing)
-                                            .foregroundColor(.white)
-                                            .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.red]), startPoint: .leading, endPoint: .trailing))
-                                            .padding(.top, 15)
-                                    
-                                        
-                                        
-                                        Image("\(spieler.photo)")
+                                    Text("\(spieler.number)")
+                                        .font(.custom("SignPainter", size: 30))
+                                        .frame(maxWidth: 350, alignment: .trailing)
+                                        .foregroundColor(.white)
+                                        .background(LinearGradient(gradient: Gradient(colors: [Color.clear, Color.red]), startPoint: .leading, endPoint: .trailing))
+                                        .padding(.top, 15)
+                                }
+                                HStack(alignment: .top, spacing: 20) {
+                                    Image("\(spieler.photo)")
                                         .resizable()
-                                        .frame(width: 100, height: 135)
+                                        .frame(width: 100, height: 135, alignment: .leading)
                                         .shadow(radius: 5)
-                                    
-                                    
-                                        
+                                    VStack(alignment: .leading, spacing: 10) {
                                         Text("\(spieler.birthDate)")
                                             .font(.custom("SignPainter", size: 16))
                                             .foregroundColor(.black)
-                                        Text("\(spieler.number)")
-                                            .font(.custom("SignPainter", size: 16))
-                                            .foregroundColor(.black)
                                         Text("\(spieler.birthPlace)")
                                             .font(.custom("SignPainter", size: 16))
                                             .foregroundColor(.black)
-                                        Text("\(spieler.birthPlace)")
+                                        Text("\(spieler.previousClub)")
                                             .font(.custom("SignPainter", size: 16))
                                             .foregroundColor(.black)
-                                        Text("\(spieler.birthPlace)")
+                                        Text("\(spieler.position)")
                                             .font(.custom("SignPainter", size: 16))
                                             .foregroundColor(.black)
-                                        
-                                            .padding(.horizontal, 200)
                                         Spacer()
                                     }
-                                    
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 0)
-                                            .fill(Color.white.opacity(0.8))
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 0)
-                                                    .stroke(
-                                                        LinearGradient(
-                                                            gradient: Gradient(colors: [.white, .red]),
-                                                            startPoint: .topLeading,
-                                                            endPoint: .bottomTrailing
-                                                        ),
-                                                        lineWidth: 3
-                                                    )
-                                            )
-                                            .shadow(radius: 0)
-                                    )
-                                    .frame(maxWidth: 380)
                                 }
+                                .padding(.horizontal, 20)
                             }
+                            .background(
+                                RoundedRectangle(cornerRadius: 0)
+                                    .fill(Color.white.opacity(0.8))
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 0)
+                                            .stroke(
+                                                LinearGradient(
+                                                    gradient: Gradient(colors: [.white, .red]),
+                                                    startPoint: .topLeading,
+                                                    endPoint: .bottomTrailing
+                                                ),
+                                                lineWidth: 3
+                                            )
+                                    )
+                                    .shadow(radius: 0)
+                            )
+                            .frame(maxWidth: 380)
                         }
                     }
                 }
             }
         }
     }
-
-
+}
 
 struct KaderView_Previews: PreviewProvider {
     static var previews: some View {
